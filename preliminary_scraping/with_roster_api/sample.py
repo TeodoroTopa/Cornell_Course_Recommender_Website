8aecfe5d6a2e226c127241cfd30ebc743c348089
Madscba <57229906+Madscba@users.noreply.github.com>
2021-04-08 08:46:55 -0400

    initial data collection

--- preliminary_scraping/with_roster_api/sample.py
+++ preliminary_scraping/with_roster_api/sample.py
@@ -1,4 +1,5 @@
 import requests
+import pandas as pd
 
 # # format: https://classes.cornell.edu/api/2.0/<method>.<responseFormat>?parameters
 # link = "https://classes.cornell.edu/api/2.0/config/rosters.json"
@@ -17,17 +18,21 @@ BASIS_URL = "https://classes.cornell.edu/api/2.0/"
 #   3. Different schools available "Engineering, Architecture and Planning" etc.
 #   4. Returns 1000, 2000, 3000, 4000 etc. (Not very useful?)
 #   5. All cornell subjects / majors (I think) "Asian American Studies,Architecture,..." etc.
-#   6.
+#   6. returns classed for a subject for a specific year
 
 METHODS = ["config/rosters","config/acadCareers","config/acadGroups","config/classLevels","config/subjects","search/classes"]
 #Potential method arguments. Ex: "roster" can be specified for method config/acadCareers
 METHODS_ARG = ["","roster","roster","roster,subject","roster,subject,acadCareer,classLevels","roster,subject,crseAttrs"]
 #Standard format for requests, XL also exists
 STD_FORMAT = ".json"
+ROSTER_PERIOD = 'SP21'
 class Roster():
     def __init__(self):
-        self.course_json = self.basic_json_extractor(method="config/rosters",parameters=None)
+        #Method 1
+        self.course_json = self.basic_json_extractor(method=METHODS[0],parameters=None)
         self.available_years_dict = self.available_rosters(self.course_json)
+        self.subjects_json = self.basic_json_extractor(method=METHODS[4],parameters=ROSTER_PERIOD)['data']['subjects']
+
 
     def basic_json_extractor(self,method="config/rosters",parameters=None):
         """
@@ -75,19 +80,34 @@ class Roster():
     #     return {"desc": r_year["descr"],"loc": r_year['defaultLocation'],"campus": r_year['defaultCampus'] }
     #
     def extract_course_rosterv0(self):
-        roster_json = self.basic_json_extractor()
-        test_year = 'SP21'
-        subjects_json = self.basic_json_extractor(method=METHODS[4],parameters=test_year)['data']['subjects']
-        test_subject = subjects_json[48]['value'] #Computer science
-
-        classes = self.basic_json_extractor(method=METHODS[5],parameters=[test_year,test_subject])
+        test_subject = self.subjects_json[48]['value'] #Computer science
+        classes = self.basic_json_extractor(method=METHODS[5],parameters=[ROSTER_PERIOD,test_subject])
         # self.extract_meta_info(roster_json,test_year)
+        data = classes['data']['classes']
+        dataFrame = pd.DataFrame.from_dict(data)
+
         a = 2
         return 0
+
+    def extract_course_rosterv1(self,year=ROSTER_PERIOD,subject=None):
+        df = pd.DataFrame()
+        if type(subject) == type(None):
+            for i in range(len(self.subjects_json)):
+                temp_sub = self.subjects_json[i]['value']
+                classes = self.basic_json_extractor(method=METHODS[5], parameters=[ROSTER_PERIOD, temp_sub])
+                temp_data = classes['data']['classes']
+                temp_dataFrame = pd.DataFrame.from_dict(temp_data)
+                df = df.append(temp_dataFrame, ignore_index=True)
+        else:
+            classes = self.basic_json_extractor(method=METHODS[5], parameters=[ROSTER_PERIOD, subject])
+            data = classes['data']['classes']
+            dataFrame = pd.DataFrame.from_dict(data)
+
+        return df
 
 
 if __name__ == "__main__":
     roster = Roster()
-
-
-    temp_data = roster.extract_course_rosterv0()
+    SP21_data = roster.extract_course_rosterv1()
+    a = 2
+    
