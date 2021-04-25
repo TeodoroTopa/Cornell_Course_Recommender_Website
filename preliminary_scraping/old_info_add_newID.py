980a604648b1721ff9284acea596c84c34318e27
yz366 <yz366@cornell.edu>
2021-04-25 15:53:48 -0400

    updated data to include ourId

--- preliminary_scraping/old_info_add_newID.py
+++ preliminary_scraping/old_info_add_newID.py
@@ -6,19 +6,6 @@ import psycopg2
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 import json
 
-
-def obtain_col_info(current_cols):
-    ret_lst = []
-
-    for key in (current_cols['binary, keep == 1'].keys()):
-        val = current_cols['binary, keep == 1'][key]
-        if (current_cols['binary, keep == 1'][key] == 1):
-            input = current_cols['column name'][key]
-            input = input[2:len(input) - 1]
-            ret_lst.append(input)
-    
-    return ret_lst
-
 def obtain_col_types(cols):
     return {
         "crseId": "INT",
@@ -67,101 +54,58 @@ def obtain_col_types(cols):
         "descrlong": "TEXT"
     }
 
-def generate_arg_string_formats(col_with_types):
-    ret = ""
-    for key in col_with_types.keys():
-        value = col_with_types[key]
-        # if (value == "TEXT"):
-        ret += "%s,"
-        # if (value == "INT"):
-        #     ret += "%d,"
-    return ret[:len(ret)-1]
+def get_data_from_db():
 
-def load_data(course_json, cols_desired, cols_with_types_dict):
-    ret = [] # list of tuples containing necessary fields
-    for course in course_json:
-        output = list()
-        for col in cols_desired:
-            if (course[col] == None):
-                if (cols_with_types_dict[col] == "TEXT"):
-                    output.append("None")
-                else:
-                    output.append(-1)
-            elif (col == "componentsRequired"): 
-                val = course[col]
-                if (isinstance(val, str)):
-                    output.append(course[col])
-                else:
-                    v = ' '.join(course[col])
-                    output.append(v)
-            else:
-                output.append(course[col])
-        ret.append(tuple(output))
-    
-    return ret
+    ''' connect to database '''
+    ''' note that these credentials may need to change '''
+    DATABASE_URL = os.environ['DATABASE_URL']
+    # url = make_url(DATABASE_URL)
+    DB_NAME = "d3ugti6oa37ph0"
+    DB_PORT = 5432
+    DB_USER = "debyvgrybtgvto"
+    DB_PASS = "255373b7ad411efa03ee0231d2c99945840266ac45b85e6144429afeea62e919"
+    DB_HOST = "ec2-52-23-45-36.compute-1.amazonaws.com"
 
-''' connect to database '''
-''' note that these credentials may need to change '''
-DATABASE_URL = os.environ['DATABASE_URL']
-# url = make_url(DATABASE_URL)
-DB_NAME = "d3ugti6oa37ph0"
-DB_PORT = 5432
-DB_USER = "debyvgrybtgvto"
-DB_PASS = "255373b7ad411efa03ee0231d2c99945840266ac45b85e6144429afeea62e919"
-DB_HOST = "ec2-52-23-45-36.compute-1.amazonaws.com"
+    conn = psycopg2.connect(dbname=DB_NAME, port=DB_PORT, user=DB_USER, password=DB_PASS, host=DB_HOST)
+    conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 
-conn = psycopg2.connect(dbname=DB_NAME, port=DB_PORT, user=DB_USER, password=DB_PASS, host=DB_HOST)
-conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    ''' creating a cursor object '''
+    cur = conn.cursor()
 
-''' creating a cursor object '''
-cur = conn.cursor()
+    ''' select - to test '''
+    cur.execute('''SELECT * FROM CLASS''')
+    result = cur.fetchall()
+    # print(result)
 
-''' load json info now '''
-# with open('ManualColumnPicking.json') as f: rows_include = json.load(f)
-# with open('course_data.json') as f: course_info = json.load(f)
+    ''' Commit your changes in the database '''
+    conn.commit()
 
-# ''' NOTE: do not forget to [heroku login] before starting '''
-# # here: extract columns that we want to put into our database
-# cols = obtain_col_info(rows_include)
+    cur.close()
+    conn.close()
 
-# # in this list, we need to assign specific types for everything
-# # returns dictionary we can then use to create a string
-# # to then form a string to input
-# cols_with_types = obtain_col_types(cols)
+    return result
 
-# cols_with_types_string = ""
-# for key in cols_with_types.keys():
-#     cols_with_types_string += key + " " + cols_with_types[key] + ","
+def add_new_id(initial_data):
+    return None
 
-# sql = "CREATE TABLE CLASS(" + cols_with_types_string[:len(cols_with_types_string)-1] + ")"
-# # print (sql)
-# cur.execute(sql)
-# print("Table created successfully........")
 
-# ''' insert into database '''
-# insert_stmt = (
-#     "INSERT INTO CLASS(" + ','.join(map(str, cols)) + ") " + 
-#     "VALUES (" + generate_arg_string_formats(cols_with_types) + ")" 
-# )
-# # print (insert_stmt)
-    
-# data=load_data(course_info, cols, cols_with_types)
+    # create a dictionary to store unique crseIDs
+    # K = (titleLong, description), and V = (crseID)
+    # this info will be appended to our current tuple
 
-# ''' Executing the SQL command '''
-# for d in data:
-#     # print (insert_stmt, d)
-#     cur.execute(insert_stmt, d)
 
-# ''' Commit your changes in the database '''
-# conn.commit()
 
-''' select - to test '''
-cur.execute('''SELECT * FROM CLASS''')
-result = cur.fetchall()
-print(result)
+def get_info_from_s3():
+    course_contents = []
+    if len(course_contents) == 0:
+        print("Retrieving course contents from s3...")
+        course_contents = get_course_data()
+    print (course_contents)
+    return course_contents
 
-''' Commit your changes in the database '''
-conn.commit()
 
-cur.close()
-conn.close()
+if __name__ == "__main__":
+    # initial_data = get_data_from_db()
+    # add_new_id(initial_data)
+    get_info_from_s3()
+
