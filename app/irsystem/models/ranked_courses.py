66294adb44a279371aef046e2c6b14ef8236c83f
mcheng1 <mkc236@cornell.edu>
2021-05-04 22:23:24 -0700

    Misspelled titles part and query as course subject and number case.

--- app/irsystem/models/ranked_courses.py
+++ app/irsystem/models/ranked_courses.py
@@ -1,13 +1,14 @@
 """
 Gets the ranked list of courses based on the cosine similarity measure 
 between the courses (using the subject, catalog number, title, and description 
-of the courses, along with the subject and catalog number together without spaces).
+of the courses).
 """
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 from app.irsystem.models.helpers import *
 import pandas as pd
 import numpy as np
+from nltk.metrics import distance
 
 class RankedCourses:
     
@@ -15,21 +16,106 @@ class RankedCourses:
         self.query = query
 
 
-    def get_ranked_course_indices(self, tf_idf_vectorizer, all_docs_tfidf):
-        """Gets the indices of the ranked courses.
+    def get_list_and_query(self, data):
+        """Gets the subject and catalog number series and the no-whitespaces query.
         """
+        subject_col = data.loc[:, 'subject']
+        catalogNbr_col = data.loc[:, 'catalogNbr']
+        
+        subject_catalogNbr_series = subject_col + catalogNbr_col
+
+        query_no_whitespaces = "".join(self.query.split())
+        
+        return (subject_catalogNbr_series, query_no_whitespaces)
+
+
+    def put_space_in_subj_catNbr_query(self, data):
+        """If the query is of the subject and catalog number, with no spaces in between,
+        then it is transformed into the subject and catalog number with a space in between.
+        """
+        subject_col = data.loc[:, 'subject']
+        catalogNbr_col = data.loc[:, 'catalogNbr']
+
+        (subject_catalogNbr_series, query_no_whitespaces) = self.get_list_and_query(data)
+
+        for i, subject_catalogNbr in enumerate(subject_catalogNbr_series):
+            if query_no_whitespaces == subject_catalogNbr:
+
+                if self.query == (subject_col[i] + catalogNbr_col[i]):
+                    
+                    self.query = subject_col[i] + " " + catalogNbr_col[i]
+    
+
+    def check_query_if_subj_course_num(self, sim_array, data):
+        """Checks if the query is the subject and the catalog number put together, 
+        whether or not there is a space between them.
+        """
+        (subject_catalogNbr_series, query_no_whitespaces) = self.get_list_and_query(data)
+        
+        for i, subject_catalogNbr in enumerate(subject_catalogNbr_series):
+            if query_no_whitespaces == subject_catalogNbr:
+                
+                sim_array[i] = 1
+        
+        return sim_array
+    
+
+    def misspelling_edit_distance(self, data):
+        """Converts the query into the course title that has the smallest 
+        edit distance with the query if the query might be of a course title 
+        that is slightly misspelled.
+        """
+        titleLong_col = data.loc[:, 'titleLong']
+
+        edit_dists = np.zeros(len(titleLong_col))
+        for i, titleLong in enumerate(titleLong_col):
+
+            edit_dists[i] = distance.edit_distance(self.query, titleLong)
+            
+
+        min_edit_dist = np.min(edit_dists)
+        min_edit_dist_index = np.argmin(edit_dists)
+        
+        min_edit_dist_threshold = len(self.query) / 5  # can be changed
+
+        if min_edit_dist <= min_edit_dist_threshold:
+            self.query = titleLong_col[min_edit_dist_index]
+        
+
+
+    def get_similarity_array(self, tf_idf_vectorizer, all_docs_tfidf, data):
+        """Gets the similarity array.
+        
+        How similar the doc is to the query is determined by the cosine similarity 
+        score.
+        """
+        self.put_space_in_subj_catNbr_query(data)
+
+        self.misspelling_edit_distance(data)
+
         query_tfidf = tf_idf_vectorizer.transform([self.query])
         sim_array = cosine_similarity(query_tfidf, all_docs_tfidf).flatten()
+        
+        
+        sim_array = self.check_query_if_subj_course_num(sim_array, data)
 
+        return sim_array
+    
+    
+    def get_ranked_course_indices(self, tf_idf_vectorizer, all_docs_tfidf, data):
+        """Gets the list of indices of the ranked courses.
+        """
+
+        sim_array = self.get_similarity_array(tf_idf_vectorizer, all_docs_tfidf, data)
         sorted_indices_all = list(np.argsort(sim_array)[::-1])
         sorted_sim_array = sorted(sim_array)[::-1]  # descending order
-
-        # only keep the indices of the courses with which the query has a 
-        # cosine similarity score of cos_sim_score or higher
-        cos_sim_score = 0.05
+        
+        # only keep the indices of the courses with which the query has the
+        # specified cosine similarity score or higher
+        cos_sim_score = 0.05  # can be changed
         sorted_indices = [index for i, index in enumerate(sorted_indices_all) if sorted_sim_array[i] >= cos_sim_score]
         return sorted_indices
-        
+
 
 def get_subj_nbr_title_desc_series(data):
     """Gets the pandas series of the course subject, catalog number, title, description combined.
@@ -42,8 +128,8 @@ def get_subj_nbr_title_desc_series(data):
     # descriptions that correspond to classes that don't have descriptions
     description_col = description_col.fillna("Not applicable.")
 
-    # the subject, catalogNbr, titleLong, and description put together, along with the subject and catalogNbr together without spaces
-    subj_nbr_title_desc_series = (subject_col + catalogNbr_col) + " " + subject_col + " " + catalogNbr_col + " " + titleLong_col + " " + description_col
+    # the subject, catalogNbr, titleLong, and description put together
+    subj_nbr_title_desc_series = subject_col + " " + catalogNbr_col + " " + titleLong_col + " " + description_col
 
     return subj_nbr_title_desc_series
 
@@ -62,53 +148,13 @@ def get_tfidf_matrix(data):
     return [vectorizer,  doc_term_tfidf_matrix]
 
 
-def get_similarity_array():
-    """Gets the similarity array.
-    
-    How similar the doc is to the query is determined by the cosine similarity 
-    score.
-    """
-
-    tf_idf = self.get_tfidf_matrix()
-
-    num_docs = len(tf_idf) - 1
-
-    query_array = tf_idf[-1,:]
-        
-    sim_array = np.zeros(num_docs)  # array of similarity scores
-    array_1 = [query_array]
-
-    for i in range(num_docs):
-            
-        array_2 = [tf_idf[i,:]]
-            
-        sim_array[i] = cosine_similarity(array_1, array_2)
-        
-    return sim_array
-        
-
-def get_ranked_course_indices():
-    """Gets the indices of the ranked courses.
-    """
-
-    sim_array = self.get_similarity_array()
-    sorted_indices = list(np.argsort(sim_array)[::-1])
-
-    # number of courses to be shown as output
-    num_courses = 15
-
-    # the indices of the most similar num_courses courses to the query
-    result = sorted_indices[:num_courses]
-    return [course_contents[index] for index in result]
-
-
 def main():
     query = "language and information"
     RankedCoursesObj = RankedCourses(query)
     course_contents = get_course_data()
     normalized_data = pd.json_normalize(course_contents)
     tf_idf_vectorizer, docs_tf = get_tfidf_matrix(normalized_data)
-    ranked_course_indices = RankedCoursesObj.get_ranked_course_indices(tf_idf_vectorizer,docs_tf)
+    ranked_course_indices = RankedCoursesObj.get_ranked_course_indices(tf_idf_vectorizer,docs_tf, normalized_data)
     print(ranked_course_indices)
 
 if __name__ == "__main__":
