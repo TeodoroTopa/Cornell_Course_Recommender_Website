8590373d68636cd672df0639f47e00c21ae453b2
yz366 <yz366@cornell.edu>
2021-05-21 00:15:06 -0400

    changing links

--- app/accounts/controllers/google_auth.py
+++ app/accounts/controllers/google_auth.py
@@ -10,7 +10,7 @@ from authlib.integrations.requests_client import OAuth2Session
 import google.oauth2.credentials
 import googleapiclient.discovery
 
-ACCESS_TOKEN_URI = 'https://oauth2.googleapis.com/token'
+ACCESS_TOKEN_URI = 'https://www.googleapis.com/oauth2/v4/token'
 AUTHORIZATION_URL = 'https://accounts.google.com/o/oauth2/v2/auth?access_type=offline&prompt=consent'
 
 AUTHORIZATION_SCOPE = 'openid email profile'
